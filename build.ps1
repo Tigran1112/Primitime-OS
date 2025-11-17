19d768bda1430e8f2a7096f3f4769c1e290a4bbe
Tigran <tarasdarovtigran@gmail.com>
2025-11-17 20:59:31 +0300

    Full fix build.ps1

--- build.ps1
+++ build.ps1
@@ -2,9 +2,22 @@ mkdir -Force build
 
 nasm -f elf32 src\load.asm -o build\load.o
 
-Get-ChildItem *.c | ForEach-Object {
-    gcc -m32 -ffreestanding -nostdlib -c $_.Name -o build\"$($_.BaseName).o"
+Get-ChildItem src\*.c -Recurse | ForEach-Object {
+    $source = $_.FullName
+    $output = "build\$($_.BaseName).o"
+    Write-Output "Compiling $source..."
+    i686-elf-gcc -m32 -ffreestanding -nostdlib -I src -c $source -o $output -Wno-implicit-function-declaration
 }
 
+$object = @()
+Get-ChildItem build\*.o -Recurse | ForEach-Object {
+    $object += $_.FullName
+}
 
-ld -m i386pe -T src\linker.ld -o build\kernel.bin build\*.o
+i686-elf-ld -T src\linker.ld -o build\kernel.elf $object
+
+i686-elf-objcopy -O binary build\kernel.elf build\kernel.bin
+
+qemu-system-i386 -kernel build\kernel.elf
+
+cls
